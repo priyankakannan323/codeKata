12c4c3a17b17801aea52c4cce71ac9fe826798df
priyankakannan323 <51689412+priyankakannan323@users.noreply.github.com>
2019-06-11 10:58:25 +0530

    Add files via upload

---
+++ repeatOrder.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <limits.h>
+int main(){
+    int n,max=INT_MIN;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    int *b=(int*)calloc(100000,sizeof(int));
+    for(i=0;i<n;i++){
+        b[a[i]]++;
+        if(max<a[i]){
+            max=a[i];
+        }
+    }
+    for(i=0;i<=max;i++){
+        if(b[i]>1){
+            printf("%d ",i);
+        }
+    }
+}
