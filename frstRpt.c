6c6c5d3ceae71f96e7c86111740a3818423f2fed
priyankakannan323 <51689412+priyankakannan323@users.noreply.github.com>
2019-06-11 13:39:00 +0530

    Add files via upload

---
+++ frstRpt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n],i,j;
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++){
+        for(j=n-1;j>i;j--){
+        if(a[i]==a[j]){
+            printf("%d",a[i]);
+            return 0;
+        }
+        }
+    }
+    if(i==j){
+        printf("unique");
+    }
+}
